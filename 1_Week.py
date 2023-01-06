06b5ba0e2b387b30aeb13db17c05505295ec3069
Changseok Jo <yulini1022@gmail.com>
2023-01-06 22:48:29 +0900

    2일차

--- 1_Week.py
+++ 1_Week.py
@@ -1,3 +1,37 @@
 # 1_Week
 
-print("hello world")
+# # 더하기
+# def plus(a, b, c):
+#     return a + b + c
+
+# # 곱하기
+# def multiply(start, end):
+#     return start * end
+
+# to = int(input("첫번째 숫자를 입력하세요 : "))
+# to2 = int(input("두번째 숫자를 입력하세요 : "))
+
+# # a = int(input("첫번째 숫자를 입력하세요 : "))
+# # b = int(input("두번째 숫자를 입력하세요 : "))
+# # c = int(input("세번째 숫자를 입력하세요 : "))
+
+# # # print("전체 합 : ", plus(a,b,c))
+# # print(f"합 : {plus(a,b,c)}")
+
+# print(f"합 : {multiply(to,to2)}")
+
+# x = int(input("첫번째 숫자를 입력하세요 : "))
+# y = int(input("두번째 숫자를 입력하세요 : "))
+
+# print("몫 : ", x // y , ", 나머지 : ", x % y)
+
+
+def factorial(n):
+    if n == 1:
+        return 1
+    
+    return n * factorial(n - 1)
+
+
+print(factorial(5))
+    
