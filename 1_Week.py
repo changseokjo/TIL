e4d5d54aae9df1e4790e045654483d4de097611b
Changseok Jo <yulini1022@gmail.com>
2023-01-09 21:44:46 +0900

    3일차

--- 1_Week.py
+++ 1_Week.py
@@ -25,13 +25,57 @@
 
 # print("몫 : ", x // y , ", 나머지 : ", x % y)
 
-
-def factorial(n):
-    if n == 1:
-        return 1
+# def factorial(n):
+#     if n == 1:
+#         return 1
     
-    return n * factorial(n - 1)
+#     return n * factorial(n - 1)
 
+# print(factorial(5))
 
-print(factorial(5))
-    
+# a, b = input("").split()
+
+# print(int(a) + int(b))
+# print(int(a) - int(b))
+# print(int(a) * int(b))
+# print(int(a) // int(b))
+# print(int(a) % int(b))
+
+# 시험 성적
+# value = int(input(""))
+
+# if 100 >= value and 89 < value:
+#     print('A')
+# elif 89 >= value and 79 < value:
+#     print('B')
+# elif 79 >= value and 69 < value:
+#     print('C')
+# elif 69 >= value and 59 < value:
+#     print('D')
+# else:
+#     print('F')
+
+# 사분면 고르기
+
+# a = input("")
+# b = input("")
+
+# if int(a) > 0:
+#     if int(b) > 0:
+#         print('1')
+#     else:
+#         print('4')
+# else:
+#     if int(b) > 0:
+#         print('2')
+#     else:
+#         print('3')
+
+# 윤년
+
+# year = input("")
+
+# if int(year) % 4 == 0 and int(year) % 100 != 0 or int(year) % 400 == 0:
+#     print('1')
+# else:
+#     print('0')
